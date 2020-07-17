02d0a777aba64a3b480bf00d55f7b7bdb27495c3
changlugen <1213691044@qq.com>
2020-07-17 16:26:03 +0800

    20200717

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// var div = document.querySelector("div");
+// console.log(div);
+
+// var a = 0;
+// var b = 1;
+// var c = 2;
+// var d = 3;
+// var e = 4;
+// var f = 5;
+// var g = 6;
+
+
+// var myVar = setInterval(function() {
+//     myTimer()
+// }, 3000);
+
+// function myTimer() {
+
+//     if (a < 6) {
+//         a++;
+//     } else {
+//         a = 0;
+//     }
+//     if (b < 6) {
+//         b++;
+//     } else {
+//         b = 0;
+//     }
+//     if (c < 6) {
+//         c++;
+//     } else {
+//         c = 0;
+//     }
+//     if (d < 6) {
+//         d++;
+//     } else {
+//         d = 0;
+//     }
+//     if (e < 6) {
+//         e++;
+//     } else {
+//         e = 0;
+//     }
+//     if (f < 6) {
+//         f++;
+//     } else {
+//         f = 0;
+//     }
+//     if (g < 6) {
+//         g++;
+//     } else {
+//         g = 0;
+//     }
+
+//     cards[a].style.transform = "translateX(0) scaleX(1) rotateY(0deg) translateZ(0px) scaleY(1)";
+//     cards[b].style.transform = "translateX(-45%) scaleX(0.54) rotateY(45deg) translateZ(-150px) scaleY(0.95)";
+//     cards[c].style.transform = "translateX(-60%) scaleX(0.51) rotateY(45deg) translateZ(-200px) scaleY(0.9)";
+//     cards[d].style.transform = "translateX(-65%) scaleX(0.48) rotateY(45deg) translateZ(-300px) scaleY(0.85)";
+//     cards[e].style.transform = "translateX(65%) scaleX(0.48) rotateY(-45deg) translateZ(-300px) scaleY(0.85)";
+//     cards[f].style.transform = "translateX(60%) scaleX(0.51) rotateY(-45deg) translateZ(-200px) scaleY(0.9)";
+//     cards[g].style.transform = "translateX(45%) scaleX(0.54) rotateY(-45deg) translateZ(-150px) scaleY(0.95)";
+// }
